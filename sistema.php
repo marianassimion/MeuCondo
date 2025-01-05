188dd8187d51a60d83f2bc84c6ebd92d1475b83b
Mariana Simion <marianasimiondossantos@gmail.com>
2025-01-05 20:09:55 -0300

    Alteração no nome dos arquivos

--- sistema.php
+++ sistema.php
@@ -16,7 +16,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SISTEMA</title>
+    <title>Moradores cadastrados</title>
     <style>
         body{
             font-family: 'Roboto', sans-serif; /* Define a fonte Roboto para todos os elementos */
--- testeLogin.php
+++ testeLogin.php
@@ -1,11 +1,9 @@
 <?php
 session_start();
-
     if (isset($_POST['submit']) && !empty($_POST['loginAdm']) && !empty($_POST['senhaAdm'])){
         include_once('config.php'); //conexão com o banco de dados
         $loginAdm = $_POST['loginAdm'];
         $senhaAdm = $_POST['senhaAdm'];
-
 
         // print_r('Login administrador: '. $loginAdm);
         // print_r('<br>');
@@ -18,18 +16,14 @@ session_start();
         if (mysqli_num_rows($result) < 1){
             unset($_SESSION['loginAdm']);
             unset($_SESSION['senhaAdm']);
-
-
             header('Location: login.php');
         }
 
         else{
             $_SESSION['loginAdm'] = $loginAdm;
             $_SESSION['senhaAdm'] = $senhaAdm;
-
             header('Location: sistema.php');
         }
-
     }
 
     else{
